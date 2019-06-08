3d9b1a0f1c95300f3398166e6a370fa60cfc15b0
baodongle <baodongle@gmail.com>
2019-06-09 02:46:29 +0700

    Add type hint for class Version

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """Semantic Versioning"""
+from functools import total_ordering
 from logging import exception
-from typing import Optional, Sequence, Tuple
+from typing import Any, Sequence, Tuple
+
+# Type annotation for Tuple of Semantic Versioning:
+VersionTuple = Tuple[int, int, int]
 
 
 # Waypoint 1:
-def convert_string_to_version_component_numbers(string: str) \
-        -> Optional[Sequence[int]]:
+def convert_string_to_version_component_numbers(string: str) -> Sequence[int]:
     """
     Convert a Semantic Versioning Component Number String to a Tuple.
 
@@ -21,24 +24,31 @@ def convert_string_to_version_component_numbers(string: str) \
         A tuple composed of 3 integers (major, minor, patch).
 
     """
+
+    def _get_valid_version(number: str) -> int:
+        """Get positive version from the number string."""
+        version = int(number)
+        if version >= 0:
+            return version
+        raise ValueError("The version must be positive!")
+
     try:
-        component = list(map(int, string.strip().split('.')))
+        component = list(map(_get_valid_version, string.strip().split('.')))
         return tuple(component[:3] + [0 for _ in range(3 - len(component))])
     except ValueError as e:
         exception('Invalid input!\n' + str(e))
-        return None
+        return 0, 0, 0
 
 
 # Waypoint 2:
-def compare_versions(this: Tuple[int, int, int], other: Tuple[int, int, int]) \
-        -> int:
+def compare_versions(this: VersionTuple, other: VersionTuple) -> int:
     """
     Compare Versions.
 
     Parameters
     ----------
-    this : Tuple[str, str, str]
-    other : Tuple[str, str, str]
+    this : VersionTuple
+    other : VersionTuple
         Both tuples composed of 3 integers (major, minor, patch) each.
 
     Returns
@@ -52,6 +62,7 @@ def compare_versions(this: Tuple[int, int, int], other: Tuple[int, int, int]) \
 
 
 # Waypoint 3:
+@total_ordering
 class Version:
     """
     Class that hold a semantic versioning 3-component number.
@@ -64,10 +75,12 @@ class Version:
         The minor version.
     patch : int
         The patch version.
+    component : VersionTuple
+        A tuple composed of 3 integers (major, minor, patch).
 
     """
 
-    def __init__(self, major, minor=0, patch=0):
+    def __init__(self, major: Any, minor: int = 0, patch: int = 0) -> None:
         self.major = 0
         self.minor = 0
         self.patch = 0
@@ -84,35 +97,44 @@ class Version:
         self.component = (self.major, self.minor, self.patch)
 
     # Waypoint 4:
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Compute "Official" String Representations."""
         return f'Version({self.major}, {self.minor}, {self.patch})'
 
     # Waypoint 5:
-    def __str__(self):
+    def __str__(self) -> str:
         """Compute "Informal" String Representation."""
         return f'{self.major}.{self.minor}.{self.patch}'
 
     # Waypoint 6: Compare `Version` Instances
-    def __lt__(self, other):
+    @staticmethod
+    def _is_valid_operand(other: Any) -> bool:
+        """Implements a form of deciding when to return NotImplemented."""
+        return all(hasattr(other, x) for x in ('major', 'minor', 'patch'))
+
+    def __lt__(self, other: Any) -> bool:
+        if not self._is_valid_operand(other):
+            return NotImplemented
         return compare_versions(self.component, other.component) == -1
 
-    def __le__(self, other):
-        return not self > other
-
-    def __eq__(self, other):
-        return compare_versions(self.component, other.component) == 0
-
-    def __ne__(self, other):
-        return not self == other
-
-    def __gt__(self, other):
-        return compare_versions(self.component, other.component) == 1
-
-    def __ge__(self, other):
-        return not self < other
+    def __eq__(self, other: Any) -> bool:
+        if not self._is_valid_operand(other):
+            return NotImplemented
+        return not compare_versions(self.component, other.component)
 
 
 if __name__ == '__main__':
-    print(repr(Version("1.2.8")))
+    print(repr(Version("1.-2.8")))
     print(Version(1, 7, 0))
+    # print(Version("1.2.8") > Version("2.4.5"))
+    # print(Version("2.4.5") > Version("1.2.8"))
+    # print(Version("1.2.8") < Version("2.4.5"))
+    # print(Version("2.4.5") < Version("1.2.8"))
+    # print(Version("2.4.5") == Version("1.2.8"))
+    # print(Version("2.4.5") == Version("2.4.5"))
+    # print(Version("2.4.5") != Version("2.4.5"))
+    # print(Version("2.4.5") != Version("1.2.8"))
+    # print(Version("2.4.5") >= Version("2.4.5"))
+    # print(Version("2.4.5") >= Version("1.2.8"))
+    # print(Version("2.4.5") <= Version("2.4.5"))
+    # print(Version("2.4.5") <= Version("1.2.8"))
